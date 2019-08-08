68dabb797ce6230235ac68cd901a625620a4cbb5
andres.serrano <aserrano@rccl.com>
2019-08-08 18:01:51 -0500

    Add Test Presenter

---
+++ app/src/test/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/CharacterPresenterTest.kt
@@ -0,0 +1,74 @@
+package com.puzzlebench.clean_marvel_kotlin.presentation.mvp
+
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import com.puzzlebench.cmk.data.mocks.factory.CharactersFactory
+import com.puzzlebench.cmk.domain.model.Character
+import io.reactivex.Single
+import io.reactivex.android.plugins.RxAndroidPlugins
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import org.junit.*
+import org.junit.Assert.*
+
+class CharacterPresenterTest {
+
+    private lateinit var presenter: CharacterPresenter
+    private var characterView = mock<CharacterView>()
+    private var characterModel = mock<CharacterModel>()
+
+    @Before
+    fun setUp() {
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler { Schedulers.trampoline() }
+        presenter = CharacterPresenter(characterView, characterModel)
+        val characters = CharactersFactory.getMockListCharacter()
+        whenever(characterModel.fetchData()).doReturn(Single.just(characters))
+    }
+
+    @Test
+    fun initPresenter() {
+        presenter.initPresenter()
+        verify(characterView).initView()
+    }
+
+    @Test
+    fun `show items`() {
+        val characters = CharactersFactory.getMockListCharacter()
+        whenever(characterModel.fetchData()).doReturn(Single.just(characters))
+        presenter.fetchData()
+        verify(characterModel).fetchData()
+        verify(characterView).showCharacters(characters)
+        verify(characterView).hideLoading()
+
+    }
+
+    @Test
+    fun `no items to show`() {
+        val noCharacter = listOf<Character>()
+        whenever(characterModel.fetchData()).doReturn(Single.just(noCharacter))
+        presenter.fetchData()
+        verify(characterModel).fetchData()
+        verify(characterView).showMessageNoItemToShow()
+        verify(characterView).hideLoading()
+
+    }
+
+    @Test
+    fun `error getting data`() {
+        val errorMessage = "mock Error Messages"
+        whenever(characterModel.fetchData()).doReturn(Single.error(Exception(errorMessage)))
+        presenter.fetchData()
+        verify(characterModel).fetchData()
+        verify(characterView).showNetworkError(errorMessage)
+        verify(characterView).hideLoading()
+
+    }
+
+    @Test
+    fun onPause() {
+        presenter.onPause()
+
+    }
+}
---
+++ app/src/test/java/com/puzzlebench/clean_marvel_kotlin/presentation/mvp/factory/CharacterFactory.kt
@@ -0,0 +1,19 @@
+package com.puzzlebench.cmk.data.mocks.factory
+
+import com.puzzlebench.cmk.domain.model.Character
+import com.puzzlebench.cmk.domain.model.Thumbnail
+
+
+class CharactersFactory {
+
+    companion object Factory {
+        const val BASE_NAME = "Name"
+        const val BASE_DESCRIPTION = "Description"
+        const val BASE_PATH = "image"
+        const val BASE_EXTENSION = ".png"
+
+        fun getMockListCharacter(): List<Character> = (1..5).map {
+            Character("$BASE_NAME$it", "$BASE_DESCRIPTION$it", Thumbnail("$BASE_PATH$it", BASE_EXTENSION))
+        }
+    }
+}
